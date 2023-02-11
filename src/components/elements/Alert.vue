2f2dfa2f18a0701f046d538b61dbde201c4786e2
serdargoleli <web.serdargoleli@gmail.com>
2023-02-11 16:13:52 +0300

    Iconlar ve alert eklendi

---
+++ src/components/elements/Alert.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import { computed } from "vue";
+  import ErrorIcon from "../icons/ErrorIcon.vue";
+  import WarningIcon from "../icons/WarningIcon.vue";
+  const props = defineProps({ type: String });
+
+  const alertType = computed(() => {
+    switch (props.type) {
+      case "warning":
+        return "alert-warning";
+      default:
+        return "alert-error";
+    }
+  });
+</script>
+<template>
+  <div :class="`alert ${alertType} shadow-lg`">
+    <div>
+      <ErrorIcon v-if="props.type == 'error'" />
+      <WarningIcon v-if="props.type == 'warning'" />
+      <span><slot></slot></span>
+    </div>
+  </div>
+</template>
